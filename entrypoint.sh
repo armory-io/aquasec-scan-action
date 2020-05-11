b8e0bc334f6dc9e7393847f33a3e9f6879fec642
Jason McIntosh <jason.mcintosh@armory.io>
2020-05-11 09:41:38 -0500

    fix(code): fix typos

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,21 +7,21 @@ REGISTRY=${4}
 IMAGE_TO_SCAN=${5}
 
 ## Login
-AQUA_TOKEN=`curl -s -X POST ${AQUA_URL}/api/v1/login -D "{ \"id\":\"${AQUA_USER}\", \"password\":\"${AQUA_PASSWORD}\"}" -H "Content-Type: application/json"|jq '.token'`
-echo "Done aquiring logins to Aquasec... using token ${AQUA_TOKEN}"
-BASE_COMMAND="curl -s -H 'Content-Type: application/json' -H \"Authorization: Bearer ${AQUA_TOKEN}\"  ${AQUA_URL}/api/v1/scanner/registry/${REGISTRY}/${IMAGE_TO_SCAN}"
-echo "Triggering a scan using ${BASE_COMMAND}/scan -X POST"
+AQUA_TOKEN=`curl -s -X POST ${AQUA_URL}/api/v1/login -d "{ \"id\":\"${AQUA_USER}\", \"password\":\"${AQUA_PASSWORD}\"}" -H "Content-Type: application/json"|jq '.token'`
+echo "Done aquiring logins to Aquasec..."
+BASE_COMMAND="curl -s -H 'Content-Type: application/json' -H \"Authorization: Bearer ${AQUA_TOKEN}\"  ${AQUA_URL}/api/v1/scanner/registry/${REGISTRY}/image/${IMAGE_TO_SCAN}"
 
 ## Trigger a scan...
+echo "Triggering a scan"
 bash -c "${BASE_COMMAND}/scan -X POST"
-echo "Scan triggered..."
 
 ## Get the status and wait for it to finish.
+echo "Checking status of the scan"
 STATUS=`bash -c "${BASE_COMMAND}/status"|jq '.status'`
-    while [[ ${STATUS} != "Scanned" ]] || [[ ${STATUS} != "Fail" ]]; do
-    STATUS=`bash -c "${BASE_COMMAND}/status"|jq '.status'`
-    echo "Build hasn't finished scanning... waiting.."
-    sleep 30
+while [[ ${STATUS} != "Scanned" ]] || [[ ${STATUS} != "Fail" ]]; do
+  STATUS=`bash -c "${BASE_COMMAND}/status"|jq '.status'`
+  echo "Got status of $STATUS ... Build hasn't finished scanning... waiting for Scanned or Fail will sleep 30 seconds"
+  sleep 30
 done
 
 bash -C "${BASE_COMMAND}/scan_result"|jq
