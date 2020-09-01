2d0e6837e554d355febfc61a1d284a801557aee8
Ethan Rogers <ethanfrogers@gmail.com>
2020-09-01 11:52:51 -0400

    feat(comment): comment on PR when scan completes

--- main.go
+++ main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
+	"html/template"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
+
+	"golang.org/x/oauth2"
+
+	"github.com/google/go-github/v32/github"
 )
 
 func main() {
@@ -45,6 +52,35 @@ func main() {
 		log.Fatalf("failed to get scan result: %v", err)
 	}
 
+	reportVulns := true
+	if v := os.Getenv("INPUT_REPORTVULNS"); v == "false" {
+		log.Println("reportVulns is false. skipping PR comment.")
+		reportVulns = false
+	}
+
+	// if true, we'll use scan data to create a comment on the commit
+	// that triggered the execution
+	if reportVulns {
+		log.Println("reporting critical and high CVE count as comment on commit.")
+		scanData, _, err := scanner.GetScanData(*registry, *image)
+		if err != nil {
+			log.Fatalf("failed to get scan data for image: %s", err.Error())
+		}
+		commentBody, err := generateReportComment(*registry, *image, *url, scanData)
+		if err != nil {
+			log.Fatalf("failed to generate comment body: %s", err.Error())
+		}
+
+		repo := strings.Split(os.Getenv("GITHUB_REPOSITORY"), "/")
+		sha := os.Getenv("GITHUB_SHA")
+		if sha == "" || os.Getenv("GITHUB_REPOSITORY") == "" {
+			log.Println("github repository and sha are required for reporting CVE counts but are empty. skipping.")
+		}
+		if err := createCommitComment(commentBody, repo[0], repo[1], sha); err != nil {
+			log.Fatalf("failed to create report comment: %s", err.Error())
+		}
+	}
+
 	if !status.Disallowed {
 		log.Println("image successfully scanned and clear of issues.")
 		return
@@ -61,11 +97,91 @@ func main() {
 
 }
 
+var noIssuesFoundTemplate = `Security Scan Results
+Image {{ .Image }} is free of critical & high CVEs.`
+
+var defaultCommentTemplate = `:warning: Security Scan Results :warning:
+Found {{ .Scan.CriticalVulns }} Critical Vulnerabilities
+Found {{ .Scan.HighVulns }} High Vulnerabilities
+
+See [scan details]({{ .AquasecHtmlImageUrl }}) for more information.
+`
+
+func createCommitComment(body []byte, owner, repo, sha string) error {
+	client := createGithubClient()
+
+	input := &github.RepositoryComment{
+		Body: github.String(string(body)),
+	}
+
+	if _, _, err := client.Repositories.CreateComment(context.Background(), owner, repo, sha, input); err != nil {
+		return err
+	}
+	return nil
+}
+
+func createGithubClient() *github.Client {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	return github.NewClient(tc)
+}
+
+func generateReportComment(registry, image, baseUrl string, sd *ScanData) ([]byte, error) {
+	// use predefined template or user defined template
+	commentTemplate := defaultCommentTemplate
+	if v := os.Getenv("INPUT_COMMENTTEMPLATE"); v != "" {
+		commentTemplate = v
+	}
+
+	// if we don't have any critical of high CVEs, fallback to the
+	// no issue template.
+	// TODO: make this template configurable in the future
+	if total := sd.CriticalVulns + sd.HighVulns; total == 0 {
+		commentTemplate = noIssuesFoundTemplate
+	}
+
+	tmpl, err := template.New("comment").Parse(commentTemplate)
+	if err != nil {
+		return nil, err
+	}
+	var b bytes.Buffer
+	splitImage := strings.SplitN(image, ":", 2)
+	encodedImage := strings.Replace(image, "/", "%2F", -1)
+	imageUrl := fmt.Sprintf("%s/#/images/%s/%s", baseUrl, registry, encodedImage)
+	type templateInput struct {
+		Registry            string
+		Image               string
+		AquasecBaseUrl      string
+		Scan                ScanData
+		ImageName           string
+		ImageTag            string
+		AquasecHtmlImageUrl string
+	}
+	if err := tmpl.Execute(&b, templateInput{
+		Registry:            registry,
+		Image:               image,
+		Scan:                *sd,
+		AquasecBaseUrl:      baseUrl,
+		ImageName:           splitImage[0],
+		ImageTag:            splitImage[1],
+		AquasecHtmlImageUrl: imageUrl,
+	}); err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), err
+}
+
 type Aquasec interface {
 	StartScan(registry, image string) error
 	WaitForScan(registry, image string) error
 	GetScanStatus(registry, image string) (string, error)
 	GetScanResult(registry, image string) (*ScanResult, string, error)
+	GetScanData(registry, image string) (*ScanData, string, error)
 }
 
 type AquasecScanner struct {
@@ -139,6 +255,33 @@ func (a *AquasecScanner) GetScanResult(registry, image string) (*ScanResult, str
 	}
 	defer resp.Body.Close()
 	var output ScanResult
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, "", err
+	}
+	if err := json.Unmarshal(b, &output); err != nil {
+		return nil, "", err
+	}
+
+	return &output, string(b), nil
+}
+
+type ScanData struct {
+	CriticalVulns int `json:"crit_vulns"`
+	HighVulns     int `json:"high_vulns"`
+	MediumVulns   int `json:"med_vulns"`
+	LowVulns      int `json:"low_vulns"`
+}
+
+func (a *AquasecScanner) GetScanData(registry, image string) (*ScanData, string, error) {
+	encodedImage := strings.Replace(image, ":", "/", 1)
+	targetUrl := fmt.Sprintf("%s/api/v2/images/%s/%s", a.baseUrl, registry, encodedImage)
+	resp, err := a.authenticatedRequest(http.MethodGet, targetUrl, nil)
+	if err != nil {
+		return nil, "", err
+	}
+	defer resp.Body.Close()
+	var output ScanData
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, "", err
